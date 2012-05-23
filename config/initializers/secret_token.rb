98de9da2b42b86d0f6ecb43e20262838bf95323d
Albanlv <albanlv@gmail.com>
2012-05-23 15:12:56 +0200

    app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CreativityNow::Application.config.secret_token = '0b78088ee089f583972c0ad33a19ddd306204e3998c6a2928caba8cb7e548f834d042f07c573fae9199411f108af0bdf2167b51b44f5b316fd65d3abdc7b4de4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CreativityNow::Application.config.session_store :cookie_store, key: '_creativity_now_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CreativityNow::Application.config.session_store :active_record_store
